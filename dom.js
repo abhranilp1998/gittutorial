2a712610b01cabeebac6a795851796d82ca8d02e
abhranilp1998 <abhranilpaul00@gmail.com>
2023-08-12 16:32:44 +0530

    dom updated

--- dom.js
+++ dom.js
@@ -12,10 +12,17 @@
 // });
 var listItems = document.getElementsByClassName("list-group-item");
 console.log("Number of list items:", listItems.length);
-listItems[2].style.background = 'green';
+listItems[1].style.background = 'lightgreen';
 for (const e of listItems) {
     e.style.fontWeight = 'bold';
 }
 
 var item = document.querySelectorAll('li');
 item[4].style.fontWeight = 'bold';
+item[4].style.display = 'none';
+item[1].style.color = 'green';
+
+var odd = document.querySelectorAll('li:nth-child(odd)');
+for (const e of odd) {
+    e.style.background = 'green';
+}
